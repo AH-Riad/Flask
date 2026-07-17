ddca3c7fb9f94d1deabaed2fa40c91acde22b58e
Riad <riadk910@gmail.com>
2026-07-17 20:51:39 +0600

    Creating home route

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/api/data', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_data():
     return jsonify({"message": "Hello, World!"})
 
